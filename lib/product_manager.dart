f8fdee2293496ddba63ff9293e15b395d62b033c
Dmitry <420600@gmail.com>
2019-04-18 22:55:52 +0500

    refactor

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,10 +3,13 @@ import './products.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[Product Manager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
+    print('[Product Manager Widget] createState');
     return _ProductManagerState();
   }
 }
@@ -16,17 +19,26 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override
   void initState() {
+    print('[Product Manager State] initState');
     _products.add(widget.startingProduct);
     super.initState();
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[Product Manager State] didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[Product Manager State] build');
     return Column(
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).primaryColor,
             onPressed: () {
               setState(() {
                 _products.add('Eater');
