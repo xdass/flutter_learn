68cfb663f9e0ddc981d7f5ed430a0964d23f7f02
dass <420600base>
2019-04-18 16:07:17 +0500

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Simple App'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Eater');
+                    });
+                  },
+                  child: Text('Add product'),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
